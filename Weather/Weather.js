8273db95f7e73c5e7573c7aa02c0477ea76ffe98
romeodecoder <romeodcoder@gmail.com>
2023-09-20 08:11:51 +0100

    getting input value and displaying on page

--- Weather/Weather.js
+++ Weather/Weather.js
@@ -1,39 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
 const apiKey = 'ec506e1477e589f7e605bdb66bbda0b3';
 const weatherInformation = document.getElementById('weather-information');
 const searchButton = document.getElementById('search-button');
 const search =document.getElementById('search');
+const locationName=document.getElementById('locationName');
+let locations ;
 
 
-function showLoader(){
-    const loader = document.getElementById('loader');
-    loader.style.display = 'flex';
-  }
-  
-  function hideloader(){
-    const loader = document.getElementById('loader');
-    loader.style.display = 'none';
-  }
-  
-  async function fetchMovieData(search){
-          showLoader();
-    try{
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`);
-      const data = await response.json()
-  
-      weatherInformation.innerHTML = ''; //clear previous results
-      console.log(data.results);
 
-        if(data.results){
+searchButton.addEventListener("click", () => {
+  locations=search.value.trim();
+  console.log(locations)
+  locationName.textContent =`Location:${locations}` ;
 
-        }
-      
-
-    }
-
-     catch{
-
-     }
-    }
+});
+});
   
   
   
